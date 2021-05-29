8ca39f8ad8f0897b2498bf9a3bb26a61ba11e83b
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-05-29 16:30:12 +0530

    Added argument parameters

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 console.log("Notes Application");
 
-const fs = require("fs");
-const chalk = require("chalk");
 const yargs = require("yargs");
 
 yargs.command({
 	command: "add",
 	describe: "Add a Note.",
-	handler: function () {
+	builder: {
+		title: {
+			describe: "Title of Note.",
+			demandOption: true,
+			type: "string",
+		},
+		body: {
+			describe: "Body of Note.",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
 		console.log("Adding a Note!!");
+		console.log("Title : ", argv.title);
+		console.log("Body : ", argv.body);
 	},
 });
 
 yargs.command({
 	command: "remove",
 	describe: "remove a Note.",
-	handler: function () {
+	builder: {
+		title: {
+			describe: "Title of Note.",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
 		console.log("removing a Note!!");
+		console.log(argv.title);
 	},
 });
 
 yargs.command({
 	command: "list",
 	describe: "list a Notes.",
-	handler: function () {
+	handler: function (argv) {
 		console.log("listing all Notes!!");
 	},
 });
@@ -31,8 +51,16 @@ yargs.command({
 yargs.command({
 	command: "read",
 	describe: "read a Note.",
-	handler: function () {
+	builder: {
+		title: {
+			describe: "Title of Note.",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: function (argv) {
 		console.log("Reading a Note!!");
+		console.log(argv.title);
 	},
 });
 
